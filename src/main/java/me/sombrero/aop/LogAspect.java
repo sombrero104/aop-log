3fc318d657da2794259fb39960faff5aa1a2dc41
sombrero104 <sombrero.khy@gmail.com>
2020-11-13 19:50:57 +0900

    .

--- src/main/java/me/sombrero/aop/LogAspect.java
+++ src/main/java/me/sombrero/aop/LogAspect.java
@@ -17,6 +17,12 @@ public class LogAspect {
 
     Logger logger = LoggerFactory.getLogger(LogAspect.class);
 
+    /**
+     * @Around 외에도..
+     *  어떤 메소드 호출 전/후, 에러가 발생했을 경우,
+     *  @annotation이 아닌 빈을 사용하여 특정 빈의 모든 메소드,
+     *  또는 표현식을 사용해서 포인트컷을 지정해 줄 수 있다.
+     */
     @Around("@annotation(LogExecutionTime)")
     public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
         StopWatch stopWatch = new StopWatch("logExecutionTime");
